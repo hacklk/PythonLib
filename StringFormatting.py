2d0a3f9bd5390cbee86c27d228522f74e1b76bb0
hacklk <32157161+hacklk@users.noreply.github.com>
2018-08-09 17:53:29 +0200

    Small Library of String Functions in  Python

---
+++ StringFormatting.py
@@ -0,0 +1,21 @@
+print("This is a string {}".format("INSERT"))
+
+print("This is {} {} {}".format("fox", "brown", "hero"))
+
+print("This is {1} {0} {2}".format("fox", "brown", "hero"))
+
+print("This is {b} {f} {h}".format(f="fox", b="a brownie", h="hero"))
+
+#{value:width.precision f}
+
+result = 100/777
+print(result)
+print("The result was {r:1.3f}".format(r=result))
+
+name = "Krisz"
+age = 33
+print("hello, his name is {}".format(name))
+print(f"{name} is {age} years old")
+
+print("{} {}".format("Python", "rules!"))
+
